1d618e92b613c7e8649abf2b50231bfc7f33f65a
JaredMartin0351 <jaredmartin0351@gmail.com>
2022-01-28 12:08:30 -0600

    added app.py file

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import redis
+import time
+
+app = Flask(__name__)
+
+cache = redis.Redis(host='redis', port=6379)
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise excretries -= 1
+            time.sleep(.5)
